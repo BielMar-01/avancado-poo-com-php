3be90eb5200139e14f28175ce040b279206f79c5
Gabriel Marques <bielmarques922@gmail.com>
2022-10-23 12:16:22 -0300

    Avançando com POO - Modulo 02 - Utilizando os namespace

--- 02-namespace-e-autoload/01-teste.php
+++ 02-namespace-e-autoload/01-teste.php
@@ -1,6 +1,10 @@
 
 <?php
 
+use Alura\Banco\Modelo\Pessoa;
+use Alura\Banco\Modelo\Funcionario;
+use Alura\Banco\Modelo\CPF;
+
 require 'src/Modelo/Pessoa.php';
 require 'src/Modelo/Funcionario.php';
 require 'src/Modelo/CPF.php';
