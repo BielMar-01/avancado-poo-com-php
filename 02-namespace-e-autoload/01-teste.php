ad91d89c4b8758baf3c0d213857dca4d6a16c513
Gabriel Marques <bielmarques922@gmail.com>
2022-10-23 12:10:30 -0300

    Avançando com POO - Modulo 02 - Namespace raiz

--- 02-namespace-e-autoload/01-teste.php
+++ 02-namespace-e-autoload/01-teste.php
@@ -1,9 +1,9 @@
 
 <?php
 
-require 'src/Pessoa.php';
-require 'src/Funcionario.php';
-require 'src/CPF.php';
+require 'src/Modelo/Pessoa.php';
+require 'src/Modelo/Funcionario.php';
+require 'src/Modelo/CPF.php';
 
 $umFuncionario = new Funcionario('Gabriel', new CPF('123.456.789-99'), 'Dev Junior');
 var_dump($umFuncionario);
