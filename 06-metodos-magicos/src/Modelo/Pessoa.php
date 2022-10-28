ee94095084120e3023d05eb3890cb7dda1037079
Gabriel Marques <bielmarques922@gmail.com>
2022-10-28 08:52:38 -0300

    Avançando com POO - Modulo 06 - Impedindo a herança

--- 06-metodos-magicos/src/Modelo/Pessoa.php
+++ 06-metodos-magicos/src/Modelo/Pessoa.php
@@ -9,7 +9,7 @@ class Pessoa
 
     public function __construct(string $nome, CPF $cpf)
     {
-        $this->validaNomeTitular($nome);
+        $this->validaNome($nome);
         $this->nome = $nome;
         $this->cpf = $cpf;
     }
@@ -25,7 +25,7 @@ class Pessoa
     }
 
 
-    protected function validaNomeTitular(string $nomeTitular)
+    final protected function validaNome(string $nomeTitular)
     {
         if (strlen($nomeTitular) < 5) {
             echo "Nome precisa ter pelo menos 5 caracteres";
