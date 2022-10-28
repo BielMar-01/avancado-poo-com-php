90aecefaa927a6fa36ffbd7191db32d525a09ca0
Gabriel Marques <bielmarques922@gmail.com>
2022-10-28 08:29:11 -0300

    Avançando com POO - Modulo 06 - Exibindo como string

---
+++ 06-metodos-magicos/01-teste.php
@@ -0,0 +1,14 @@
+
+<?php
+
+require_once 'autoload.php';
+
+use Alura\Banco\Modelo\Endereco;
+
+
+$endereco1 = new Endereco('São Paulo', 'Bairro Um', 'Rua Um', '171');
+$endereco2 = new Endereco('Rio de Janeiro', 'Bairro Um e Dois', 'Rua Dois', '171A');
+
+echo $endereco1 . PHP_EOL;
+echo $endereco2;
+
